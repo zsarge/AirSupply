8bbe19cd2dc94c5f46eb0b8707d2a86682f12b09
Zack Sargent <zackarywsargent@gmail.com>
2020-11-26 14:28:50 -0500

    Format comments

--- AirSupply2/AirSupply2.ino
+++ AirSupply2/AirSupply2.ino
@@ -1,40 +1,38 @@
+// This sketch operates a fresh air vent. The fresh air resupplies the house from exhaust fans and combustion air.
+// Arduino's heat call pin should be connected to Normally Open side of the furnace switch/relay.
+// When there is a furnace call for heat, the furnace relay closes and heatCallReading will read HIGH.
+// Arduino also opens the vent if there is a call from exhaust fans.
 //
-//This sketch operates a fresh air vent. The fresh air resupplies the house from exhaust fans and combustion air.
-//Arduino's heat call pin should be connected to Normally Open side of the furnace switch/relay.
-//When there is a furnace call for heat, the furnace relay closes and heatCallReading will read HIGH.
-//Arduino also opens the vent if there is a call from exhaust fans.
+// Vent position is determined by three magnetic switches attached to the vent door. Signals are sent to Arduino.
+// Vent door overtravel is prevented by two mechanical roller microswitches (one on each side) wired with the relays.
 //
-//Vent position is determined by three magnetic switches attached to the vent door. Signals are sent to Arduino.
-//Vent door overtravel is prevented by two mechanical roller microswitches (one on each side) wired with the relays.
-//
-//The signal from the furnace is sent from a solenoid relay, which can send "dirty" signals that need debouncing.
+// The signal from the furnace is sent from a solenoid relay, which can send "dirty" signals that need debouncing.
 
-//When first turned on, the arduino attempts to determine if the vent is open, closed, or in the middle
+// When first turned on, the arduino attempts to determine if the vent is open, closed, or in the middle
 
 
-
-//vars
-//pins
-//const int otherCallPin = 4;      // digital pin number, input from non-furnace needs for air. Sets the ventCall value
+// vars
+// pins
+// const int otherCallPin = 4;      // digital pin number, input from non-furnace needs for air. Sets the ventCall value
 const int heatCallPin = 5;      // digital pin number, input from heat call relay
 const int maxOpenLimitSw = 6;   // max open limit switch. Switch (NC) opens when vent is open at max. LOW when vent is open, HIGH when closed.
 const int maxClosedLimitSw = 7; // max closed limit switch. Switch (NC) opens when vent is closed at max. LOW when vent is closed, HIGH when open.
-//const int midLimitSw =8;        // middle limit switch. Switch is closed when vent is close to the middle.
+// const int midLimitSw =8;        // middle limit switch. Switch is closed when vent is close to the middle.
 const int rlyOpenVent = 9;      // relay control to open the vent
 const int rlyCloseVent = 10;    // relay control to close the vent
 
 
-//values
-//int ventReading;                 // stores the present actual state of the vent door 0 = damper closed  1 = damper midway  2 = damper full open
+// values
+// int ventReading;                 // stores the present actual state of the vent door 0 = damper closed  1 = damper midway  2 = damper full open
 int heatCall = 0;            // Is there a call for heat?
-//int ventCall = 0;            // Other than the furnace, is there something calling for an open vent?
-//int otherCallReading = 0;        // present reading from otherCallPin
+// int ventCall = 0;            // Other than the furnace, is there something calling for an open vent?
+// int otherCallReading = 0;        // present reading from otherCallPin
 int heatCallReading = 0;             // present reading from heatCallPin
 int lastHeatCallReading = 0;         // stores last reading from heatCallPin
-//int maxOpenLimitSwReading;   // reading from open limit switch
-//int maxClosedLimitSwReading; // reading from max closed limit switch
-//int midLimitSwReading;       // reading from mid limit switch
-//int firstTimeRun = true;     // tracks if this is the first time this has run (ie after a power outage)
+// int maxOpenLimitSwReading;   // reading from open limit switch
+// int maxClosedLimitSwReading; // reading from max closed limit switch
+// int midLimitSwReading;       // reading from mid limit switch
+// int firstTimeRun = true;     // tracks if this is the first time this has run (ie after a power outage)
 
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 10000;   // the debounce time; increase if the output flickers
@@ -99,10 +97,10 @@ void handleHeatCall(int heatCall) {
 
 void loop() {
 
-  //int ventCall= digitalRead(otherCallPin);
-  //int maxOpenLimitSwReading = digitalRead(maxOpenLimitSw);
-  //int maxClosedLimitSwReading = digitalRead();
-  //int midLimitSwReading = digitalRead();
+  // int ventCall= digitalRead(otherCallPin);
+  // int maxOpenLimitSwReading = digitalRead(maxOpenLimitSw);
+  // int maxClosedLimitSwReading = digitalRead();
+  // int midLimitSwReading = digitalRead();
 
   // check for heat call, debounce signal
 
